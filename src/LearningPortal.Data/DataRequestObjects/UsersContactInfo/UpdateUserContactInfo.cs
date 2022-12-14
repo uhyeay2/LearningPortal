96ca65b50ebe18689254f225000bac5525fa4fc1
Daniel Aguirre <uhyeay24@gmail.com>
2022-12-14 12:48:54 -0600

    Working on UpdateUserContactInfo

---
+++ src/LearningPortal.Data/DataRequestObjects/UsersContactInfo/UpdateUserContactInfo.cs
@@ -0,0 +1,49 @@
+﻿namespace LearningPortal.Data.DataRequestObjects.UsersContactInfo
+{
+    public class UpdateUserContactInfo : IDataRequestObject
+    {
+
+        #region Constructor
+
+        public UpdateUserContactInfo(Guid userGuid, string? firstName = null, string? lastName = null, string? preferredName = null, string? email = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PreferredName = preferredName;
+            Email = email;
+            UserGuid = userGuid;
+        }
+
+        #endregion
+
+        #region Properties To Update
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+        
+        public string? PreferredName { get; set; }
+
+        public string? Email { get; set; }
+
+        public Guid UserGuid { get; set; }
+
+        #endregion
+
+        #region IDataRequestObject Obligations
+
+        public object? GenerateParameters() => new {FirstName, LastName, PreferredName, Email, UserGuid};
+
+        public string GenerateSql() => _sql;
+
+        private static readonly string _sql =
+        $@"
+            DECLARE @UserId int = ( {Fetch.Query(Table.Users_NoLock, "Id", where: "Guid = @UserGuid")} )
+            
+            {Update.Coalesce(Table.UsersContactInfo, where: "UserId = @UserId", 
+                items: new[] {"FirstName", "LastName", "PreferredName", "Email"} )}
+        ";
+            
+        #endregion
+    }
+}
---
+++ tests/LearningPortal.Data.Tests/UserContactInfoTests/UpdateUserContactInfoTests.cs
@@ -0,0 +1,26 @@
+﻿using LearningPortal.Data.DataRequestObjects.UsersContactInfo;
+
+namespace LearningPortal.Data.Tests.UserContactInfoTests
+{    
+    public class UpdateUserContactInfoTests : BaseDataTest
+    {
+        [Fact]
+        public async Task UpdateUserContactInfo_Given_NoUserExistsWithGuid_Should_ReturnZero() =>
+            Assert.Equal(0, await _data.ExecuteAsync(new UpdateUserContactInfo(Guid.NewGuid())));
+
+        [Fact]
+        public async Task UpdateUserContactInfo_Given_UserExistsWithGuid_Should_ReturnOne() 
+        {
+            var identity = "UpdateUserContactInfoUserExistsReturnsOne";
+            var userGuid = Guid.NewGuid();
+
+            await _seeder.SeedUserAndContactInfo(identity, userGuid);
+
+            var result = await _data.ExecuteAsync(new UpdateUserContactInfo(userGuid));
+
+            await _remover.RemoveUserAndContactInfoByIdentity(identity);
+
+            Assert.Equal(1, result);
+        }
+    }
+}
